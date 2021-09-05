3248bdd74d5379984b70a87147901fdd85075bfb
Ajeet <ajeetbharti279@gmail.com>
2021-09-05 15:22:02 +0530

    Datatype

---
+++ Hello.js
@@ -0,0 +1,17 @@
+var d1="Hello JavaScript"
+var name='Rahul'
+var Name='Kumar'
+var NAME='Sibli'
+console.log(name)
+console.log(Name)
+console.log(NAME)
+
+var data
+console.log(data)
+
+// console.log(d1)
+// console.log(d1)
+// console.log(d1)
+// console.log(d1)
+// console.log(d1)
+// console.log(d1)
