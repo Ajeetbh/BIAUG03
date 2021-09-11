35b02b7dc36830c869f3e6abc5360093ae9f005e
Ajeet <ajeetbharti279@gmail.com>
2021-09-11 11:01:12 +0530

    If, if else

--- Hello.js
+++ Hello.js
@@ -1,31 +1,58 @@
-var d1="Hello JavaScript"
-var name='Rahul'
-var Name='Kumar'
-var NAME='Sibli'
-console.log(name)
-console.log(Name)
-console.log(NAME)
+// var a=10
+// if(a%2==0){
+//     console.log(a+"is Even")
+// }
+// else{
+//     console.log(a+"is Odd")
+// }
+// var q=99
+// if(q>100){
+//     console.log(q+"is greater than 100")
+// }else if(q<100){
+//     if(q>90){
+//         console.log(q+"is greater than 90")
+//     }else if(q<90){
+//         console.log(q+"is smaller than 90")
+//     }else{
+//         console.log(q+"is eqaul t0 90")
+//     }
+//     console.log(q+"is smaller than 100")
+// }else{
+//     console.log(q+"is eqaul t0 100")
+// }
 
-var data
-console.log(data)
+// var n='10'
+// if(n==10){
+//     console.log(5)
+// }else{
+//     console.log(6)
+// }
 
-// console.log(d1)
-// console.log(d1)
-// console.log(d1)
-// console.log(d1)
-// console.log(d1)
-// console.log(d1)
-fac=10
-console.log(fac)
-var undefined=10
-console.log(undefined)
-var none=15
-console.log(none)
-var a=10
-var b=20
-console.log(a+b)
-console.log(a-b)
-console.log(a*b)
-console.log(a/b)
-console.log(a%b)
-console.log(a**b)
+// var n='10'
+// if(n===10){
+//     console.log(55)
+// }else{
+//     console.log(66)
+// }
+
+var m='jan'
+if(m=='feb'){
+    console.log(m+"has 28 days")
+}
+if(m=='jan'||m=='mar'||m=='jul'||m=='aug'||m=='oct'||m=='dec'){
+    console.log(m+'has 31 days')
+}else if(m=='apr' ||m=='jun'||m=='sep'||m=='nov'){
+    console.log(m+'has 30 days')
+}
+
+
+
+
+
+
+
+
+
+
+
+
