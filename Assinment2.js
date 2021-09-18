4c0053621c196b561b58dfec03927d887e9cd9af
Ajeet <ajeetbharti279@gmail.com>
2021-09-18 17:52:36 +0530

    Assignment2 complete

--- Assinment2.js
+++ Assinment2.js
@@ -290,3 +290,31 @@
 //     return answer;
 //   }
 //   console.log(sequentialSizes(8));
+
+// 29 function chainToSwitch(val) {
+//     var answer = "";
+//     switch(val){
+//     case "bob": 
+//       answer = "Marley";
+//       break;
+//     case 42: 
+//       answer = "The Answer";
+//       break;
+//     case 1:
+//       answer = "There is no #1";
+//       break;
+//     case 99:
+//       answer = "Missed me by this much!";
+//       break;
+//     case 7:
+//       answer = "Ate Nine";
+//       break
+//     }
+//     return answer;
+//   }
+//   console.log(chainToSwitch(7));
+
+function isLess(a, b) {
+   return a<b
+ }
+ console.log(isLess(10, 15));
