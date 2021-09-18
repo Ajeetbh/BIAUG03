16c249245840ecd789c278d174a65f44e7a01d9a
Ajeet <ajeetbharti279@gmail.com>
2021-09-18 11:55:33 +0530

    Assignment1

--- function.js
+++ function.js
@@ -1,37 +1,45 @@
 // function jarvis(){
 //     console.log("Hey I am jarvis")
 //     for(var a =1 ; a<4 ; a++){
-
 //         console.log(a)
 //     }
 //     console.log("Bye Bye from Jarvis")
 // }
-
-// function eat(a, b){
+// jarvis()
+// function eat(a,b){
 //     if(a=="breakfast"){
 //         console.log("Bread")
 //     }  
+//     if(b=='morningTea'){
+//         console.log('Coffie or tea')
+//     }
 //     if(a=="lunch"){
 //         console.log("Idli")
+//     }
+//     if(b=='drink'){
+//         console.log('water')
 //     }
 //     if(a=="dinner"){
 //         console.log("Pizza")
 //     }  
-//     console.log(b)
 // }
+// eat('breakfast','morningTea')
+// eat('lunch','drink')
+// eat('dinner','drink')
 // function greet(name){
 //     var msg="Hey "+name+", Welcome to BI"
 //     console.log(msg)
 //     //intead of printing this, I want to return the message to the place
 //     //from where it was called
 // }
-
 // greet("Rahul")
 
-// function square(num){
-//     var result=num*num
-//     return result
-// }
+function square(num){
+    var result=num*num
+    return result
+}
+var s=square(6)
+console.log(s)
 // function printNumberLessThanTen(number){
 //     for(var a =number ; a<10 ; a++){
 
@@ -51,10 +59,10 @@
 // var a="rahul"
 // console.log(a)
 
-var a = 10
-function rand(){
-     a="rahul"
-    console.log(a)
-}
-rand()
-console.log(a)
+// var a = 10
+// function rand(){
+//      a="rahul"
+//     console.log(a)
+// }
+// rand()
+// console.log(a)
