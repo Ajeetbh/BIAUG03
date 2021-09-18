a6530aa26ad20e6068339c241ca8303e98a67f3e
Ajeet <ajeetbharti279@gmail.com>
2021-09-18 10:40:49 +0530

    loop

---
+++ loop.js
@@ -0,0 +1,82 @@
+// console.log("Hello 2021")
+// console.log("Hiii")
+// console.log("Hello 2021")
+// console.log("Hiii")
+// console.log("Hello 2021")
+// console.log("Hiii")
+// console.log("Hello 2021")
+// console.log("Hiii")
+// console.log("Hello 2021")
+// console.log("Hiii")
+// console.log("Hello 2021")
+// console.log("Hiii")
+// console.log("Hello 2021")
+// console.log("Hiii")
+// console.log("Hello 2021")
+// console.log("Hiii")
+// console.log("Hello 2021")
+// console.log("Hiii")
+// console.log("Hello 2021")
+// console.log("Hiii")
+// console.log("Hello 2021")
+// console.log("Hiii")
+// console.log("Hello 2021")
+// console.log("Hiii")
+// console.log("Hello 2021")
+// console.log("Hiii")
+
+
+/**
+ 3-Types of loop
+ 1. while loop
+SYNTAX:
+    while(condition){
+        Statement
+        increment/decrement
+        Statement
+        Statement
+        Statement
+        Statement
+    }
+ */
+// var a=10
+// while(a>8){
+//     console.log("Hello 2021")
+//     console.log("Hiii")
+//     a++
+//     console.log(a)
+// }
+// // if(0){
+//     console.log("hi")
+// }
+// var a=1
+// do{
+//     a++
+//     console.log(a)
+// }while(a>"4apple")
+
+// console.log(a<"4apple")
+
+
+/**
+ for(initialization ; condition ; icr/decr){
+ }
+ */
+
+
+
+// for(var a =1 ; a<4 ; a++){
+
+//     console.log(a)
+// }
+
+
+var a=1
+while(a<4){
+    console.log(a)
+    a++
+}
+
+/**
+ function name(){
+ }**/
