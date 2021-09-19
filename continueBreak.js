90930d785c8e7da7d761536362f3082b56b1d44f
Ajeet <ajeetbharti279@gmail.com>
2021-09-19 12:36:43 +0530

    Continue Break

--- continueBreak.js
+++ continueBreak.js
@@ -141,15 +141,15 @@
 //     console.log(a)
 // } 1 3 1
 
-for(var a=1;a<4;a++){
-    if(a==2){
-        continue;
-    }
-    for(var b=1;b<4;b++){
-        if(b==2){
-            break;
-        }
-        console.log(b)
-    }
-    console.log(a)
-} 
+// for(var a=1;a<4;a++){
+//     if(a==2){
+//         continue;
+//     }
+//     for(var b=1;b<4;b++){
+//         if(b==2){
+//             break;
+//         }
+//         console.log(b)
+//     }
+//     console.log(a)
+// } 1 1 1 3
