77fcf953dbbf1d4543405624c760a0e9ac1dd0cc
Ajeet <ajeetbharti279@gmail.com>
2021-10-02 13:39:39 +0530

    Array 1

--- array.js
+++ array.js
@@ -1,1 +1,112 @@
-console.log('Array')
+//var data=[] //empty array
+// var data=[1,2,3,4,5,6,7]
+// console.log(data.length)
+//index: start from 0
+//data: 0 to 6    for last=length-1
+// console.log(data[6])  //7
+
+// console.log(data[30]) //undefined
+
+//console.log(data[200]==undefined)  //true
+// var data1=new Array()
+// console.log(data1.length)
+
+//var data=['rahul','kumar',12,false,null,undefined,{}]
+//console.log(data.length)
+// data[7]='node'
+// console.log(data.length)//8
+// console.log(data)
+// data[10]='node'
+// console.log(data.length) //11
+// console.log(data[10])
+// console.log(data)  ['rahul','kumar',12,false,null,undefined,{},<3 empty items>,'node']
+
+// data.length=2
+// console.log(data)  //rahul kumar
+// data.length=20
+// console.log(data)  //update the data
+//                    //rahul,kumar,<18 empty items>
+// console.log(data.length)  //20
+
+// var x=[1,2,5]
+// console.log(x.length)
+// var y=[3,4]
+// console.log(y.length)
+// var z=x+y
+// console.log(z.length)
+
+//STACK
+// var stack=[1,2,3,4,5]
+// stack.push(12)
+// console.log(stack)  //add the data at end
+// stack.pop()
+// console.log(stack)   //delete the data form end
+
+// stack.unshift(15)  // add 15 to begning of array
+// console.log(stack)
+// stack.shift()
+// console.log(stack) // delete form begining
+ 
+// console.log(stack.indexOf(3)) //4
+// console.log(stack.indexOf(40)) //-1
+//  1. find the index of Array
+//  2. to know whether the given element in there in array or not
+
+//QUEUE
+//var queue=['ajeet','ashitha','jayesh','kailash','ayyappa']
+//insert tha data in queue-from rear(back,end)--push
+//delete the data form queue- from the front(begning)--shift
+// queue.push('milten')
+// queue.shift()
+// console.log(queue) //[ 'ashitha', 'jayesh', 'kailash', 'ayyappa', 'milten' ]
+
+
+//Splice
+//delete the range of element from the array
+var mark=[1,12,14,17,18,0]
+//mark.splice(0,3) //0 is the index,3 number from 0th index will be deleted
+// console.log(mark) //[17,18,0]
+// mark.splice(3,1)
+// console.log(mark) //[1,12,14,18,0]
+ 
+var age=[19,18,10]
+//age.splice(index,0,ele1,ele2,ele3..)
+// age.splice(0,0,1,2,3)
+// console.log(age)  //[1,2,3,19,18,10]
+// age.splice(1,0,1,2,3) //[19,1,2,3,18,10]
+// age.splice(2,0,1,2,3) //[19,18,1,2,3,10]
+// age.splice(4,0,1,2,3)//[19,18,10,1,2,3]
+// age.splice(6,0,1,2,3)// [19,18,1,2,3]
+
+// age.splice(1,1,1,2)//[19,1,2,10]
+// console.log(age);
+// age.splice(1,3,1,2)//
+// console.log(age)//[19,1,2]
+// age.splice(0,3,1,2)//[1,2]
+// console.log(age)
+
+//SLICE
+// var age=[1,2,3,4,5,11,6,7,8,9,0]
+// var newAge=age.slice(2,5) //start from index 2 to 4
+// console.log(newAge)//[3,4,5]
+// var newAge=age.splice(2,3) [1,2,11,6,7,8,9,0]
+ 
+//Sorting
+//var age=[10,1,3,2,5,4,7,9,6]
+// age.sort()
+// console.log(age)
+
+// console.log('10'<'2')
+
+ var age=[10,1,3,2,5,4,7,9,6]
+// age.sort(function(a,b){
+//     if(a>b){
+//         return 1
+//     }
+//     if(a<b){
+//         return -1
+//     }
+// })//passing a function to a function is called callback function
+
+age.sort((a,b)=>a-b) // arrow function
+console.log(age)
--- assingment3.js
+++ assingment3.js
@@ -1,1 +1,153 @@
-console.log('Hi')
+//BASIC JAVASCRIPT--String
+//1. Declare String Variables
+// let myFirstName='Ajeet'
+// let myLastName='Bharti'
+// console.log(myFirstName)
+// console.log(myLastName)
+
+//2. Escaping Literal Quotes in Strings
+// var myStr = "I am a \"double quoted\" string inside \"double quotes\"."; 
+// console.log(myStr)
+
+//3. Quoting Strings with Single Quotes
+// var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+// console.log(myStr)
+
+//4 Escape Sequences in Strings
+// var myStr="FirstLine\n\t\\SecondLine\nThirdLine"; 
+// console.log(myStr)
+
+//5. Concatenating Strings with Plus Operator
+// var myStr="This is the start. "+"This is the end.";
+// console.log(myStr) 
+
+//6. Concatenating Strings with the Plus Equals Operator
+// var myStr= "This is the first sentence. ";
+// myStr += "This is the second sentence.";
+// console.log(myStr)
+
+//7 Constructing Strings with Variables
+// var myName='Ajeet Kumar Bhari';
+// var myStr='My name is'+myName+'I am well!';
+// console.log(myStr)
+
+//8 Appending Variables to Strings
+// var someAdjective='Hi Hello Good';
+// var myStr = "Learning to code is ";
+// myStr += someAdjective
+// console.log(myStr)
+
+//9 Find the Length of a String
+// var lastNameLength = 0;
+// var lastName = "Lovelace";
+// lastNameLength = lastName.length;
+// console.log(lastNameLength)
+
+//10 Use Bracket Notation to Find the First Character in a String
+// var firstLetterOfLastName = "";
+// var lastName = "Lovelace";
+// firstLetterOfLastName = lastName[0]; 
+// console.log(firstLetterOfLastName)
+
+//11  Understand String Immutability
+// var myStr = "Jello World";
+// myStr[0] = "H";
+// console.log(myStr)
+
+//12 Use Bracket Notation to Find the Nth Character in a String
+// var lastName = "Lovelace";
+// var thirdLetterOfLastName = lastName[2]; 
+// console.log(thirdLetterOfLastName)
+
+//13 Use Bracket Notation to Find the Last Character in a String
+// var lastName = "Lovelace";
+// var lastLetterOfLastName = lastName[lastName.length-1]; 
+// console.log(lastLetterOfLastName)
+
+//14 Use Bracket Notation to Find the Nth-to-Last Character in a String
+// var lastName = "Lovelace";
+// var secondToLastLetterOfLastName = lastName[lastName.length-2]; 
+// console.log(secondToLastLetterOfLastName)
+
+// 15 Word Blanks
+// var myNoun = "dog";
+// var myAdjective = "big";
+// var myVerb = "ran";
+// var myAdverb = "quickly";
+// var wordBlanks = "The "+" "+myAdjective+" "+myNoun+" "+myVerb+" "+myAdverb+"."; 
+// console.log(wordBlanks)
+
+
+//BASIC JAVASCRIPT OBJECT
+
+//1. Build JavaScript Objects
+// var myDog = {
+//     name:'Monty',
+//     legs:4,
+//     tails:1,
+//     friends:['men','police']
+//  };
+// console.log(myDog)
+
+//2.Accessing Object Properties with Dot Notation
+// var testObj = {
+//     "hat": "ballcap",
+//     "shirt": "jersey",
+//     "shoes": "cleats"
+//   };
+//   var hatValue = testObj.hat;      
+//   var shirtValue = testObj.shirt; 
+//   console.log(hatValue)
+//   console.log(shirtValue) 
+
+//3 Accessing Object Properties with Bracket Notation
+// var testObj = {
+//     "an entree": "hamburger",
+//     "my side": "veggies",
+//     "the drink": "water"
+//   };
+//   var entreeValue = testObj['an entree'];  
+//   var drinkValue = testObj['the drink'];   
+//   console.log(entreeValue)
+//   console.log(drinkValue)
+
+//4 Accessing Object Properties with Variables
+// var testObj = {
+//     12: "Namath",
+//     16: "Montana",
+//     19: "Unitas"
+//   };
+//   var playerNumber=16;       
+//   var player = testObj[playerNumber];   
+//   console.log(player)
+
+//5 Updating Object Properties
+// var myDog = {
+//     "name": "Coder",
+//     "legs": 4,
+//     "tails": 1,
+//     "friends": ["freeCodeCamp Campers"]
+//   };
+//   myDog['name']='Happy Coder'
+//   console.log(myDog)
+
+//6 Add New Properties to a JavaScript Object
+// var myDog = {
+//     "name": "Happy Coder",
+//     "legs": 4,
+//     "tails": 1,
+//     "friends": ["freeCodeCamp Campers"]
+//   };
+//   myDog.bark='woof'
+//   console.log(myDog)
+  
+//7 Delete Properties from a JavaScript Object
+// var myDog = {
+//     "name": "Happy Coder",
+//     "legs": 4,
+//     "tails": 1,
+//     "friends": ["freeCodeCamp Campers"],
+//     "bark": "woof"
+//   };
+//   delete myDog.tails
+//   console.log(myDog)
